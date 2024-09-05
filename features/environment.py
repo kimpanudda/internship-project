8999259ee3f0422a81081c1171991d51e9419b85
Kim Panudda <kimpanudda@gmail.com>
2024-09-04 17:18:34 -0700

    Integrate your tests with a Cloud Testing Platform

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 from app.application import Application
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
@@ -18,10 +19,28 @@ def browser_init(context):
     # options.add_argument('headless')
     # service = Service(ChromeDriverManager().install())
     # context.driver = webdriver.Chrome(service=service, options=options)
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
-    context.driver = webdriver.Firefox(service=service)
+    # context.driver = webdriver.Firefox(service=service)
+
+    ### BROWSERSTACK ###
+    bs_user = 'kimp_kXaRcM'
+    bs_key = 'U5n6Y5H1iSavF1hurNsF'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+        'browserName': 'Firefox',
+        'sessionName': scenario_name
+    }
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    context.driver.set_window_size(1920, 1080) # for only headless mode and browserstack
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -30,18 +49,19 @@ def browser_init(context):
     context.app = Application(context.driver)
 
     ### HEADLESS MODE FIREFOX ####
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    service = Service(GeckoDriverManager().install())
-    context.driver = webdriver.Chrome(
-        options=options,
-        service=service
-    )
+    # options = webdriver.FirefoxOptions()
+    # options.add_argument('--headless')
+    # service = Service(GeckoDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    # browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
