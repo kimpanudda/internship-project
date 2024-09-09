e584ebea9252370717ad7faa0ad15bb2c3931930
Kim Panudda <kimpanudda@gmail.com>
2024-09-09 14:13:16 -0700

    Adding a report tool

--- features/environment.py
+++ features/environment.py
@@ -11,36 +11,41 @@ def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    # driver_path = ChromeDriverManager().install()
+    ### CHROME ####
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
 
-    # context.driver = webdriver.Chrome(service=service)
-
+    # ### FIREFOX ####
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+    ### HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
     # service = Service(ChromeDriverManager().install())
-    # context.driver = webdriver.Chrome(service=service, options=options)
-    # driver_path = GeckoDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
-    # context.driver = webdriver.Firefox(service=service)
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
 
     ### BROWSERSTACK ###
-    bs_user = 'kimp_kXaRcM'
-    bs_key = 'U5n6Y5H1iSavF1hurNsF'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # bs_user = 'kimp_kXaRcM'
+    # bs_key = 'U5n6Y5H1iSavF1hurNsF'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os": "Windows",
+    #     "osVersion": "11",
+    #     'browserName': 'Firefox',
+    #     'sessionName': scenario_name
+    # }
+    #
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
-    options = Options()
-    bstack_options = {
-        "os": "Windows",
-        "osVersion": "11",
-        'browserName': 'Firefox',
-        'sessionName': scenario_name
-    }
-
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
-
-    context.driver.set_window_size(1920, 1080) # for only headless mode and browserstack
+    # context.driver.set_window_size(1920, 1080) # for only headless mode and browserstack
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
